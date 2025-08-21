4f3717891349031f8517cf7671fd8bcad4062312
Vadym <kirilkovadim193@gmail.com>
2025-08-21 17:29:12 +0300

    add Plant model, and GardenBed model with plant FK. Also added str methods

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class GardenBed(models.Model):
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    is_processed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Gardenbed #{self.id} — {self.plant.name}"
